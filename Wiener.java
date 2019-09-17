3a3c464f5e84afc1409d95dc85f5a05041d36912
MICHAL-SOBIERAJ <michalsobieraj966@wp.pl>
2019-09-17 21:27:32 +0200

    added plotting, documentation and input params

--- Wiener.java
+++ Wiener.java
@@ -2,15 +2,34 @@ import java.util.Random;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 
+/** @author ZeroMaster28 
+* Class represents Wiener processs sample path which is 
+* written to a file 'data.txt' using basic BufferedWriter.
+* In a final phase the path is drawn via gnuplot.
+*
+* @version 1.0
+* Gnuplot drawing phase avaiable only on unix-like systems */
 public class Wiener{
 
-     static final double T=1.0d;
-     static final int N=10000;
+     static double T=1.0d;
+     static int N=10000;
      static double h=T/N;
      static boolean isWindows;
 
      public static void main(String []args){
         
+	if(args!=null)
+	{
+		if(args.length==1)
+		{
+			N=Integer.parseInt(args[0]);	
+		}
+		if(args.length==2)
+		{
+			T=Integer.parseInt(args[0]);
+			N=Integer.parseInt(args[1]);
+		}
+	}
         double sum=0.0d;
         Random rand=new Random();
 		try{
@@ -31,6 +50,9 @@ public class Wiener{
 		}
 	useGnuplot();
      }
+     
+     /**Draws data from file via Gnuplot
+      * Plotting_options should contain shell input for gnuplot*/
      static void useGnuplot()
      {
 
@@ -41,8 +63,7 @@ public class Wiener{
 	} 
 	else {
 	try{
-    	process = Runtime.getRuntime().exec("gnuplot < plotting_options");
-	Thread.currentThread().sleep(10000);
+    	process = Runtime.getRuntime().exec("gnuplot < plotting_options -p");
 	   }
 	catch(Exception ex)
 	   {
